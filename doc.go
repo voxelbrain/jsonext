59cf30df5e2f5c3db1f8b23d5bdd7c1a84d1c30e
Alexander Surma <surma@surmair.de>
2013-08-02 23:05:26 +0200

    Add documentation

---
+++ doc.go
@@ -0,0 +1,6 @@
+// jsonext is an drop-in replacement for encoding/json.
+// Note: The API of `encoding/json` has not been completely mirrored yet.
+// As of now, it is not an actual drop-in replacement.
+//
+// The documentation is equivalent to encoding/json, unless otherwise noted.
+package jsonext
